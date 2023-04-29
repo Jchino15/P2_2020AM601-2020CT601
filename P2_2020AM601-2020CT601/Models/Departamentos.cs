382ae71e9545c586c2e2b38deaf5b6f2a46be4ca
Miguel Córdova <miguel.cordova@catolica.edu.sv>
2023-04-29 13:28:08 -0600

    Cambios Modelos

--- P2_2020AM601-2020CT601/Models/Departamentos.cs
+++ P2_2020AM601-2020CT601/Models/Departamentos.cs
@@ -5,7 +5,6 @@ namespace P2_2020AM601_2020CT601.Models
     public class Departamentos
     {
         [Key]
-
         public int Iddepartamento { get; set; }
 
         public string? nombreDepartamento { get; set; }
--- P2_2020AM601-2020CT601/Models/Generos.cs
+++ P2_2020AM601-2020CT601/Models/Generos.cs
@@ -5,7 +5,6 @@ namespace P2_2020AM601_2020CT601.Models
     public class Generos
     {
         [Key]
-
         public int Idgenero { get; set; }
         public string? genero { get; set; }
     }
