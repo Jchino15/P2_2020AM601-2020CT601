940aa4e757cb8210b02cf9a2c66a09232709d617
Miguel Córdova <miguel.cordova@catolica.edu.sv>
2023-04-29 08:29:59 -0600

    CRUDS con el asistente

---
+++ P2_2020AM601-2020CT601/Models/Generos.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P2_2020AM601_2020CT601.Models
+{
+    public class Generos
+    {
+        [Key]
+
+        public int Idgenero { get; set; }
+        public string? genero { get; set; }
+    }
+}
--- P2_2020AM601-2020CT601/Program.cs
+++ P2_2020AM601-2020CT601/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using P2_2020AM601_2020CT601.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<covidcontext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("COVIDConnection")
+    )
+);
 
 var app = builder.Build();
 
