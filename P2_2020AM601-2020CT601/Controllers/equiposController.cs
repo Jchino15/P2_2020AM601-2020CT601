b7e15cf0a6dd58370a5aded7ab20aee33315ebcf
Josue Alvarado <josue.jdam12@gmail.com>
2023-04-29 08:30:36 -0600

    Josue

---
+++ P2_2020AM601-2020CT601/Controllers/equiposController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P2_2020AM601_2020CT601.Controllers
+{
+    public class equiposController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
