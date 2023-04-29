aa31d2b5f6952222a29ca2b0c5cba385a6358a04
Josue Alvarado <josue.jdam12@gmail.com>
2023-04-29 07:42:25 -0600

    Agregar archivos de proyecto.

---
+++ P2_2020AM601-2020CT601/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P2_2020AM601_2020CT601.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
