ee0e97ecac7a192e52f38d772e0a625443d3679c
Josue Alvarado <josue.jdam12@gmail.com>
2023-04-29 09:44:18 -0600

    Probar

--- P2_2020AM601-2020CT601/Controllers/equiposController.cs
+++ P2_2020AM601-2020CT601/Controllers/equiposController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using P2_2020AM601_2020CT601.Models;
 
 namespace P2_2020AM601_2020CT601.Controllers
 {
@@ -6,7 +8,12 @@ namespace P2_2020AM601_2020CT601.Controllers
     {
         public IActionResult Index()
         {
+            var listadoDepartamentos = (from d in covidcontext.Departamentos select d).ToList();
+
+            ViewData["listadoDepartamentos"] = new SelectList(listadoDepartamentos, "Iddepartamento", "nombreDepartamento");
+
             return View();
         }
     }
 }
+    
