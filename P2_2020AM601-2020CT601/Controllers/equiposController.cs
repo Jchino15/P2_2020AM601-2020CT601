76a41783ee583f22c3a251403b7e1bfe86506b94
Josue Alvarado <josue.jdam12@gmail.com>
2023-04-29 09:54:30 -0600

    Controller update

--- P2_2020AM601-2020CT601/Controllers/equiposController.cs
+++ P2_2020AM601-2020CT601/Controllers/equiposController.cs
@@ -6,9 +6,10 @@ namespace P2_2020AM601_2020CT601.Controllers
 {
     public class equiposController : Controller
     {
+        private readonly covidcontext _covidcontext;
         public IActionResult Index()
         {
-            var listadoDepartamentos = (from d in covidcontext.Departamentos select d).ToList();
+            var listadoDepartamentos = (from d in _covidcontext.Departamentos select d).ToList();
 
             ViewData["listadoDepartamentos"] = new SelectList(listadoDepartamentos, "Iddepartamento", "nombreDepartamento");
 
