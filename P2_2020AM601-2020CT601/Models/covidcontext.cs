1aa4ebdbdfb4ea88839740f96a4a3c222adb6821
Miguel Córdova <miguel.cordova@catolica.edu.sv>
2023-04-29 13:15:52 -0600

    Casos Reportados

--- P2_2020AM601-2020CT601/Models/covidcontext.cs
+++ P2_2020AM601-2020CT601/Models/covidcontext.cs
@@ -10,5 +10,7 @@ namespace P2_2020AM601_2020CT601.Models
         { }
         public DbSet<Departamentos> Departamentos { get; set; }
         public DbSet<Generos> Generos { get; set; }
+
+        public DbSet<casosReportados> casosReportados { get;set; }
     }
 }
